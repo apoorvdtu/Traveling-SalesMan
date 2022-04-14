3f6a5b6b9125cde8fd1220102de0c50b2715f608
apoorvdtu <apoorvambavat_co20b3_07@dtu.ac.in>
2022-04-14 11:55:13 +0530

    Added Restart Feature

--- index.js
+++ index.js
@@ -5,19 +5,37 @@
   }).addTo(map);
   var polyarr = [];
   var latlngs = [];
-
+  var polyarrow = [];
   let run = document.querySelector('.run');
-
+  let restart = document.querySelector('.restart');
+  restart.addEventListener('click', function () {
+    for (let i = 0; i < polyarr.length; i++) {
+      polyarr[i].removeFrom(map);
+    }
+    for (let i = 0; i < polyarrow.length; i++) {
+      polyarrow[i].removeFrom(map);
+    }
+    polyarr = [];
+    polyarrow = [];
+    latlngs = [];
+    map.eachLayer(function (layer) {
+      map.removeLayer(layer);
+    });
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    
+  })
   const icon = L.icon({
     iconUrl: 'backpack.jpg',
     iconSize: [80, 60]
   });
 
-  const marker = L.marker([15.5937, 78.9629], {
-    icon
-  })
-  // marker.bindPopup('<h4>Travler</h4>')
-  marker.addTo(map);
+  // const marker = L.marker([15.5937, 78.9629], {
+  //   icon
+  // })
+  // // marker.bindPopup('<h4>Travler</h4>')
+  // marker.addTo(map);
   map.addEventListener('click', addMarkerToMap);
 
 
@@ -31,23 +49,67 @@
     }
     let count = tsp(0, graph, 1, visited, "0", 0, 0);
     console.log(ans);
+
     console.log(count);
-    paths = [];
+    let minCost = Infinity;
+    let minpsf = "";
+    for (let i = 0; i < ans.length; i++) {
+      let cost = ans[i].cost;
+      let psf = ans[i].psf;
+      if (minCost > cost) {
+        minCost = cost;
+        minpsf = psf;
+      }
+    }
+    count = 0;
+    printAllPaths();
+    // let cid = setInterval(function () {
+    //   if (count == 5) {
+    //     clearInterval(cid);
+    //   }
+    //   count++;
+    // }, 1000)
+    // var path = [];
+    // for (let i = 0; i < minpsf.length; i++) {
+    //   let markernum = parseInt(minpsf[i]);
+    //   path.push(latlngs[markernum]);
+    // }
+    // latlngs = [];
+    // latlngs = path.filter(() => {
+    //   return true;
+    // });
+    // path = [];
+    // addPolyLineToGraph();
+    console.log(minCost + "," + minpsf);
     graph = [];
     ans = [];
+
     console.log("--------------");
     // tsp_dp(graph);
 
   }
+
   var ans = [];
   function tsp(i, graph, csf, visited, psf, cost, msrc) {
     if (csf == graph.length) {
       psf = psf + " " + msrc;
       psf = psf.trim().split(" ");
+
+      var path = [];
+      for (let i = 0; i < psf.length; i++) {
+        let markernum = parseInt(psf[i]);
+        path.push(latlngs[markernum]);
+      }
+      latlngs = path.filter(() => {
+        return true;
+      });
+      path = [];
+      addPolyLineToGraph();
       ans.push({
         psf: psf,
         cost: cost + graph[i][msrc]
       })
+
       return 1;
     }
     let count = 0;
@@ -69,27 +131,64 @@
     marker.addTo(map);
 
     latlngs.push([e.latlng.lat, e.latlng.lng]);
-    
+
     marker.addEventListener('click', function () {
       map.removeLayer(marker);
 
-     let latidx =  latlngs.findIndex((latlng)=>{
-        return  latlng[0] == e.latlng.lat && latlng[1] == e.latlng.lng
+      let latidx = latlngs.findIndex((latlng) => {
+        return latlng[0] == e.latlng.lat && latlng[1] == e.latlng.lng
       })
-      latlngs.splice(latidx,1);
-      for(let i = 0;i<polyarr.length;i++)
-      {
-          polyarr[i].removeFrom(map);
-      }
-      polyarr =[];
+      latlngs.splice(latidx, 1);
+      // for(let i = 0;i<polyarr.length;i++)
+      // {
+      //     polyarr[i].removeFrom(map);
+      // }
+      // polyarr =[];
       addPolyLineToGraph();
     })
     addPolyLineToGraph();
   }
-  function addPolyLineToGraph()
-  {
+
+  function printAllPaths() {
+    for (let i = 0; i < ans.length; i++) {
+      let cost = ans[i].cost;
+      let psf = ans[i].psf;
+      var path = [];
+      for (let i = 0; i < psf.length; i++) {
+        let markernum = parseInt(psf[i]);
+        path.push(latlngs[markernum]);
+      }
+      latlngs = [];
+      latlngs = path.filter(() => {
+        return true;
+      });
+      addPolyLineToGraph();
+      count = 0;
+      let cid = setInterval(function () {
+        if (count == 5) {
+          clearInterval(cid);
+        }
+        count++;
+      }, 1000);
+
+    }
+
+  }
+
+  function addPolyLineToGraph() {
+    for (let i = 0; i < polyarr.length; i++) {
+      polyarr[i].removeFrom(map);
+    }
+    for (let i = 0; i < polyarrow.length; i++) {
+      polyarrow[i].removeFrom(map);
+    }
+    polyarr = [];
+    polyarrow = [];
     var polyline = L.polyline(latlngs, { color: 'red' });
     polyline.addTo(map);
+    let polyarrows = L.featureGroup(getArrows(latlngs, 'black', 2, map));
+    polyarrows.addTo(map)
+    polyarrow.push(polyarrows);
     polyarr.push(polyline);
   }
   function fill2dGraph() {
@@ -128,5 +227,87 @@
   function toRadian(degree) {
     return degree * Math.PI / 180;
   }
-  
+  function getArrows(arrLatlngs, color, arrowCount, mapObj) {
+
+    if (typeof arrLatlngs === undefined || arrLatlngs == null ||
+      (!arrLatlngs.length) || arrLatlngs.length < 2)
+      return [];
+
+    if (typeof arrowCount === 'undefined' || arrowCount == null)
+      arrowCount = 1;
+
+    if (typeof color === 'undefined' || color == null)
+      color = '';
+    else
+      color = 'color:' + color;
+
+    var result = [];
+    for (var i = 1; i < arrLatlngs.length; i++) {
+      var icon = L.divIcon({ className: 'arrow-icon', bgPos: [5, 5], html: '<div style="' + color + ';transform: rotate(' + getAngle(arrLatlngs[i - 1], arrLatlngs[i], -1).toString() + 'deg)">▶</div>' });
+      for (var c = 1; c <= arrowCount; c++) {
+        result.push(L.marker(myMidPoint(arrLatlngs[i], arrLatlngs[i - 1], (c / (arrowCount + 1)), mapObj), { icon: icon }));
+      }
+    }
+    return result;
+  }
+
+  function getAngle(latLng1, latlng2, coef) {
+    var dy = latlng2[0] - latLng1[0];
+    var dx = Math.cos(Math.PI / 180 * latLng1[0]) * (latlng2[1] - latLng1[1]);
+    var ang = ((Math.atan2(dy, dx) / Math.PI) * 180 * coef);
+    return (ang).toFixed(2);
+  }
+
+  function myMidPoint(latlng1, latlng2, per, mapObj) {
+    if (!mapObj)
+      throw new Error('map is not defined');
+
+    var halfDist, segDist, dist, p1, p2, ratio,
+      points = [];
+
+    p1 = mapObj.project(new L.latLng(latlng1));
+    p2 = mapObj.project(new L.latLng(latlng2));
+
+    halfDist = distanceTo(p1, p2) * per;
+
+    if (halfDist === 0)
+      return mapObj.unproject(p1);
+
+    dist = distanceTo(p1, p2);
+
+    if (dist > halfDist) {
+      ratio = (dist - halfDist) / dist;
+      var res = mapObj.unproject(new Point(p2.x - ratio * (p2.x - p1.x), p2.y - ratio * (p2.y - p1.y)));
+      return [res.lat, res.lng];
+    }
+
+  }
+
+  function distanceTo(p1, p2) {
+    var x = p2.x - p1.x,
+      y = p2.y - p1.y;
+
+    return Math.sqrt(x * x + y * y);
+  }
+
+  function toPoint(x, y, round) {
+    if (x instanceof Point) {
+      return x;
+    }
+    if (isArray(x)) {
+      return new Point(x[0], x[1]);
+    }
+    if (x === undefined || x === null) {
+      return x;
+    }
+    if (typeof x === 'object' && 'x' in x && 'y' in x) {
+      return new Point(x.x, x.y);
+    }
+    return new Point(x, y, round);
+  }
+
+  function Point(x, y, round) {
+    this.x = (round ? Math.round(x) : x);
+    this.y = (round ? Math.round(y) : y);
+  }
 })();
